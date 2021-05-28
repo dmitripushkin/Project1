25934cc73c8b920ca80a9184e71163510285f4a2
Dmitri Gordeev <dimapushkin@gmail.com>
2021-05-28 20:06:15 +0300

    Initial Commit

---
+++ Project1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Повелитель on 28.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
